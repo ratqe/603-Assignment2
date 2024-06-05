c5ca7f930b925bf8856ca14d12854d3182dd80f9
vexmomo <79814427+vexmomo@users.noreply.github.com>
2024-06-05 15:26:55 +1200

    Code for assignment 2 (v1)
    in this version i have taken all the features we made in assignment one and presented them all on a GUI, in the next version we will try to add more features like a login screen, a button to go back to the menu screen, etc.

---
+++ pkg603_assignment/LanguageQuestions.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg603_assignment;
+
+/**
+ *
+ * @author User
+ */
+import java.util.List;
+
+
+interface LanguageQuestions {
+    static List<String[]> getQuestions() {
+        return null;
+    }
+}
+
+
